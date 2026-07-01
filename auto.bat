2dbbcdb416cae4a2feb806f225a7bc957c638f6f
luong gia phat <luonggiaphat07@gmail.com>
2026-07-01 15:20:27 +0700

    Update code: Wed 07/01/2026 15:20:27.76

--- auto.bat
+++ auto.bat
@@ -2,17 +2,15 @@
 title Cap nhat Source Code
 cd /d "%~dp0"
 
-:: Xóa lock file nếu có
+:: Xóa lock file
 if exist ".git\index.lock" del ".git\index.lock"
 
-:: Xóa sạch cache index (để đảm bảo Git không giữ lại các tệp cũ)
+:: Xóa sạch cache, chỉ add file code
 git rm -r --cached .
-:: Chỉ thêm đúng các file code thiết yếu
 git add app.py train.py auto.bat requirements.txt runtime.txt .gitignore
 
-:: Commit và push
+:: Commit và đẩy
 git commit -m "Update code: %date% %time%"
-git push origin main
-
-echo [+] XONG! Da day code len GitHub thanh cong!
+git push -u origin main
+echo [+] XONG! Da day code len GitHub!
 pause
